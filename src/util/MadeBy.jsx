e80f81791d80ca4808f9349fbbbeb3835d5a5780
Durgeshkeshri <84857957+Durgeshkeshri@users.noreply.github.com>
2024-02-24 20:58:39 +0530

    made by changes

--- src/util/MadeBy.jsx
+++ src/util/MadeBy.jsx
@@ -5,20 +5,21 @@ import Ujjwal from '../../image/ujjwal.jpg'
 import rishit from '../../image/rishit.jpg'
 import sakshi from '../../image/sakshi.jpg'
 import danish from '../../image/danish.jpg'
+import parikha from '../../image/parikha.jpg'
 import CardSlider from './CardSlider';
 const MadeBy = ({ isSmallScreen }) => {
   const slides = [
-    {
-      image: `${DurgeshKeshri}`,
-      name: "DURGESH KESHRI",
-      role: "(DEVELOPER)",
-      year: "SY BCA"
-    },
     {
       image: `${Ujjwal}`,
       name: "UJJWAL SINGH",
       role: "(UI/UX)",
       year: "TY CSE"
+    },
+    {
+      image: `${DurgeshKeshri}`,
+      name: "DURGESH KESHRI",
+      role: "(DEVELOPER)",
+      year: "SY BCA"
     },
     {
       image: `${kritansh}`,
@@ -27,9 +28,15 @@ const MadeBy = ({ isSmallScreen }) => {
       year: "TY CSE"
     },
     {
+      image: `${parikha}`,
+      name: "PARIKHA KATHANE",
+      role: "(BROCHURE LOGO)",
+      year: "TY CSBS"
+    },
+    {
       image: `${rishit}`,
       name: "RISHIT SRIVASTAVA",
-      role: "(LOGO)",
+      role: "(WEBSITE LOGO)",
       year: "SY CSE"
     },
     {
@@ -60,14 +67,8 @@ const MadeBy = ({ isSmallScreen }) => {
 
       <div className="mt-8 flex justify-center max-md:hidden">
 
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid grid-cols-4 gap-8">
 
-          <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
-            <img src={DurgeshKeshri} alt="" className='w-[200px] h-[200px] rounded-3xl' />
-            <p className='text-center text-white text-lg font-homo font-bold'>DURGESH KESHRI</p>
-            <p className='-mt-1 text-center text-white text-sm font-bold'>(DEVELOPER)</p>
-            <p className='-mt-1 text-center text-white text-sm font-bold'>SY BCA</p>
-          </div>
           <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
             <img src={Ujjwal} alt="" className='w-[200px] h-[200px] rounded-3xl' />
             <p className='text-center text-white text-lg font-homo font-bold'>UJJWAL SINGH</p>
@@ -75,16 +76,19 @@ const MadeBy = ({ isSmallScreen }) => {
             <p className='-mt-1 text-center text-white text-sm font-bold'>TY CSE</p>
           </div>
           <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
-            <img src={kritansh} alt="" className='w-[200px] h-[200px] rounded-3xl' />
+            <img src={DurgeshKeshri} alt="" className='w-[200px] h-[200px] rounded-3xl' />
+            <p className='text-center text-white text-lg font-homo font-bold'>DURGESH KESHRI</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>(DEVELOPER)</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>SY BCA</p>
+          </div>
+          <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
+            <a href="https://linktr.ee/kritansh_tank">
+              <img src={kritansh} alt="" className='w-[200px] h-[200px] rounded-3xl' />
+            </a>
             <p className='text-center text-white text-lg font-homo font-bold'>KRITANSH TANK</p>
             <p className='-mt-1 text-center text-white text-sm font-bold'>(DEVELOPER)</p>
             <p className='-mt-1 text-center text-white text-sm font-bold'>TY CSE</p>
-          </div>
-          <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
-            <img src={rishit} alt="" className='w-[200px] h-[200px] rounded-3xl' />
-            <p className='text-center text-white text-lg font-homo font-bold'>RISHIT SRIVASTAVA</p>
-            <p className='-mt-1 text-center text-white text-sm font-bold'>(LOGO)</p>
-            <p className='-mt-1 text-center text-white text-sm font-bold'>TY CSE</p>
+            
           </div>
           <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
             <img src={danish} alt="" className='w-[200px] h-[200px] rounded-3xl' />
@@ -92,17 +96,30 @@ const MadeBy = ({ isSmallScreen }) => {
             <p className='-mt-1 text-center text-white text-sm font-bold'>(DEVELOPER)</p>
             <p className='-mt-1 text-center text-white text-sm font-bold'>SY BCA</p>
           </div>
+        </div>
+      </div>
+
+      <div className="mt-8 flex justify-center max-md:hidden">
+        <div className="grid grid-cols-3 gap-8">
+          <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
+            <img src={parikha} alt="" className='w-[200px] h-[200px] rounded-3xl' />
+            <p className='text-center text-white text-lg font-homo font-bold'>PARIKHA KATHANE</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>(BROCHURE LOGO)</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>TY CSBS</p>
+          </div>
+          <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
+            <img src={rishit} alt="" className='w-[200px] h-[200px] rounded-3xl' />
+            <p className='text-center text-white text-lg font-homo font-bold'>RISHIT SRIVASTAVA</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>(WEBSITE LOGO)</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>SY CSE</p>
+          </div>
           <div className="w-[250px] h-[250px] flex flex-col gap-2 items-center mb-6">
             <img src={sakshi} alt="" className='w-[200px] h-[200px] rounded-3xl' />
             <p className='text-center text-white text-lg font-homo font-bold'>SAKSHI SAWANT</p>
             <p className='-mt-1 text-center text-white text-sm font-bold'>(CONVERSATIONAL DESIGN)</p>
-            <p className='-mt-1 text-center text-white text-sm font-bold'>TY CSE</p>
+            <p className='-mt-1 text-center text-white text-sm font-bold'>FINAL YEAR CSE</p>
           </div>
-
-
-
         </div>
-
       </div>
       <div>
         {isSmallScreen && (
